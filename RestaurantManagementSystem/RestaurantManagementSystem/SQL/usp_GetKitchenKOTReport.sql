8ccd72fe654b37ede0124b8cc5cdae65fe11fea5
apporel27-collab <ap.porel27@gmail.com>
2025-10-19 16:37:13 +0530

    chore: commit all local changes

--- RestaurantManagementSystem/RestaurantManagementSystem/SQL/usp_GetKitchenKOTReport.sql
+++ RestaurantManagementSystem/RestaurantManagementSystem/SQL/usp_GetKitchenKOTReport.sql
@@ -1,17 +1,5 @@
--- Stored procedure: usp_GetKitchenKOTReport
--- Parameters: @FromDate DATE = NULL, @ToDate DATE = NULL, @Station NVARCHAR(100) = NULL
--- Returns: KOT items for kitchen display/export
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-IF OBJECT_ID('dbo.usp_GetKitchenKOTReport', 'P') IS NOT NULL
-    DROP PROCEDURE dbo.usp_GetKitchenKOTReport
-GO
-
-CREATE PROCEDURE dbo.usp_GetKitchenKOTReport
+--EXEC [dbo].[usp_GetKitchenKOTReport] ''
+create or alter PROCEDURE [dbo].[usp_GetKitchenKOTReport]
     @FromDate DATE = NULL,
     @ToDate DATE = NULL,
     @Station NVARCHAR(100) = NULL
@@ -22,22 +10,23 @@ BEGIN
     DECLARE @Start DATETIME = COALESCE(CAST(@FromDate AS DATETIME), DATEADD(day, -1, CAST(GETDATE() AS DATE)));
     DECLARE @End DATETIME = DATEADD(day, 1, COALESCE(CAST(@ToDate AS DATETIME), CAST(GETDATE() AS DATE)));
 
-    SELECT
+    SELECT Distinct
         o.Id AS OrderId,
         o.OrderNumber,
         ISNULL(t.TableName, CONCAT('Table ', o.TableTurnoverId)) AS TableName,
         i.Name AS ItemName,
         oi.Quantity,
         ISNULL(s.Name, '') AS Station,
-        CASE WHEN oi.IsPrepared = 1 THEN 'Completed' ELSE 'Pending' END AS Status,
-        oi.RequestedAt
+        CASE WHEN kti.status = 3 THEN 'Completed' ELSE 'Pending' END AS Status,
+        kti.StartTime
     FROM OrderItems oi
-    INNER JOIN Orders o ON oi.OrderId = o.Id
-    LEFT JOIN MenuItems i ON oi.MenuItemId = i.Id
-    LEFT JOIN Stations s ON i.StationId = s.Id
+    INNER JOIN dbo.Orders o ON oi.OrderId = o.Id
+    LEFT JOIN dbo.MenuItems i ON oi.MenuItemId = i.Id
+    LEFT JOIN [dbo].[KitchenStations] s ON i.KitchenStationId = s.Id
     LEFT JOIN Tables t ON o.TableTurnoverId = t.Id
-    WHERE oi.RequestedAt >= @Start AND oi.RequestedAt < @End
+    Left Join [purojit2_idmcbp].[KitchenTicketItems] kti on kti.OrderItemId=oi.id
+    WHERE kti.StartTime >= @Start AND kti.CompletionTime < @End
     AND (@Station IS NULL OR @Station = '' OR s.Name = @Station)
-    ORDER BY oi.RequestedAt DESC;
+    ORDER BY kti.StartTime DESC;
 END
 GO
