aab50a201075a1bec52ad00d7d6c10d9dcafc613
apporel27-collab <ap.porel27@gmail.com>
2025-10-23 22:17:52 +0530

    Make SubCategory required on Menu Create/Edit (UI + ViewModel)

--- RestaurantManagementSystem/RestaurantManagementSystem/ViewModels/MenuViewModels.cs
+++ RestaurantManagementSystem/RestaurantManagementSystem/ViewModels/MenuViewModels.cs
@@ -32,8 +32,9 @@ namespace RestaurantManagementSystem.ViewModels
         [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
-        [Display(Name = "Sub Category")]
-        public int? SubCategoryId { get; set; }
+    [Required(ErrorMessage = "Sub Category is required")]
+    [Display(Name = "Sub Category")]
+    public int? SubCategoryId { get; set; }
 
         [Display(Name = "Image")]
         public string ImagePath { get; set; }
